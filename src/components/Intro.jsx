cafd36c375e356f2fabd88760b9e0be168cb695d
Brian Nguyen <nguyen@Brians-MBA15.local>
2025-04-14 01:01:56 -0700

    update intro

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -7,12 +7,12 @@ const Intro = ({ navigate }) => {
 		<div id="intro" onMouseOver={navigate}>
 			<div className="heading">
 				<Responsive maxWidth={800}>
-					<h1 className="titles">SOFTWARE DEVELOPER</h1>
+					<h1 className="titles">SOFTWARE ENGINEER</h1>
 					<h1 className="titles">LIFELONG LEARNER</h1>
 					<h1 className="titles">TECH ENTHUSIAST</h1>
 				</Responsive>
 				<Responsive minWidth={801}>
-					<h1 className="titles">SOFTWARE DEVELOPER • LIFELONG LEARNER • TECH ENTHUSIAST</h1>
+					<h1 className="titles">SOFTWARE ENGINEER • LIFELONG LEARNER • TECH ENTHUSIAST</h1>
 				</Responsive>
 			</div>
 		</div>
